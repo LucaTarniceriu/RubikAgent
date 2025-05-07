e8f159208e020d3a8ceccf4353aa5be4c777a874
Luca Tarniceriu <luca.tarniceriu04@e-uvt.ro>
2025-05-07 23:02:37 +0300

    update q function template

--- solver.py
+++ solver.py
@@ -1,4 +1,9 @@
 from cubeSimulator import *
+alpha = 0.2
+gamma = 0.95
 
-def Q(state, action):
-    return Q(state, action) + alpha * ( reward(state, action) + gamma * Q(nextState, nextAction) - Q(state, action))
+state = myCube
+action = 'r'
+nextState = moveCube(myCube, 'r')
+q = [{}]
+q[state][action] = q[state][action] + alpha * (reward(state, action) + gamma * max(q[nextState]) - q[state][action])
